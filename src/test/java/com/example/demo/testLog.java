d531a81a266bd8273d2465d45f0080ec339597bd
nick <trojanhorse0077@gmail.com>
2021-10-05 00:52:25 +0800

    Initial commit

---
+++ src/test/java/com/example/demo/testLog.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+
+@Slf4j
+public class testLog {
+    @Test
+    void name() {
+        log.info("aa");
+    }
+}
